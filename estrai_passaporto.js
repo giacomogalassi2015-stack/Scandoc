ea8e7a81af5a6251ed2116a530934a98eb276efb
Giacomo Galassi <giacomogalassi2015@example.com>
2026-02-25 21:50:07 +0100

    red

--- estrai_passaporto.js
+++
@@ -1,5 +0,0 @@
-// estrai_passaporto.js
-function estraiDatiPassaporto(lines) {
-    console.log("Logica passaporti (MRZ 2 righe da 44) da implementare in futuro.");
-    return null; 
-}
--- estrai_patente.js
+++
@@ -1,5 +0,0 @@
-// estrai_patente.js
-function estraiDatiPatente(testoGrezzo) {
-    console.log("Logica patenti (lettura campi numerati 1, 2, 3...) da implementare in futuro.");
-    return null;
-}
